c97c73e2b4549a71aac43c15e6c42805181cfee3
rayaneacacio <rray74307@gmail.com>
2023-08-11 11:19:33 -0300

    navegação da pagina

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 import { HeartButton } from "../heartButton";
@@ -8,13 +10,19 @@ import { Container, Span } from "./style";
 export function Card({ img, title, price }) {
   let number = "01";
 
+  const navigate = useNavigate();
+
+  function navigateToFood() {
+    navigate("/food");
+  }
+
   return (
     <Container>
       <HeartButton id="heartButton" />
 
       <img src={ img } alt={ `imagem de ${title}` } />
 
-      <button id="titleButton"> {` ${ title } >`} </button>
+      <button id="titleButton" onClick={ navigateToFood }> {` ${ title } >`} </button>
 
       <span id="price"> R$ { price } </span>
 
