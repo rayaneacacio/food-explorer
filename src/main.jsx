4269bcd5bb2422c9c461531a24636f4249e03d91
rayaneacacio <rray74307@gmail.com>
2023-08-11 22:52:43 -0300

    importaçao do AdminProvider para as rotas da aplicaçao

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import { ThemeProvider } from 'styled-components';
 import theme from './style/theme';
 import GlobalStyles from './style/global';
 
+import { AdminProvider } from './hooks/isAdmin';
+
 import Routes from "./routes/index";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AdminProvider>
+        <Routes />
+      </AdminProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
