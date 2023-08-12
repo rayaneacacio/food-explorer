2c6f004d73884cfbda27bd5a7c4700e72361f066
rayaneacacio <rray74307@gmail.com>
2023-08-11 22:10:59 -0300

    novas cores

--- src/style/theme.js
+++ src/style/theme.js
@@ -4,19 +4,22 @@ export default {
     BACKGROUND_700: "#000A0F", //background da pagina (body)
     BACKGROUND_500: "#001119", //background do header
     BACKGROUND_555: "#00111A", //background do footer
-    BACKGROUND_300: "#0D1D25", //background dps inputs
+    BACKGROUND_333: "#0D161B", //background dos inputs das pages de criar e editar pratos
+    BACKGROUND_300: "#0D1D25", //background dos inputs
 
     BORDER: "#000204", //border dos cards
     BORDER_290: "#192227", //border do h2
 
     GRAY: "#C4C4CC", //titulo dos inputs
+    GRAY_300:"#76797B", //background de tags
     GRAY_500: "#7C7C8A", //placeholder dos inputs
     GRAY_700: "#4D585E", //logo no footer
 
     WHITE: "#FFF",
     WHITE_25: "#E1E1E6", //h2 (tópicos)
 
-    RED: "#750310",
+    RED: "#750310", //buttons
+    RED_02: "#AB4D55", //button save (botao de criar e editar pratos)
 
     BLUE: "#82F3FF", //preços
   },
